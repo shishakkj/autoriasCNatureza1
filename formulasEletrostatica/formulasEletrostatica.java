ddc2047252d73865a19fee99221271d9e441f1c9
Shisha <102593026+shishakkj@users.noreply.github.com>
2023-04-26 09:50:15 -0300

    adicionando try e catch para o sistema não crashar

--- formulasEletrostatica/formulasEletrostatica.java
+++ formulasEletrostatica/formulasEletrostatica.java
@@ -2,6 +2,16 @@ import java.util.Scanner;
 public class Main
 {
 	public static void main(String[] args) {
+        try{
+            escolha();
+        } 
+        catch(Exception e){
+          System.out.print("Por favor digite um valor válido\n");
+          escolha();
+        }
+	}
+	
+	public static void escolha(){
 	    Scanner sla = new Scanner(System.in);
 	    System.out.println("Escolha uma fórmula para utilizar: \n[1] - Carga elétrica\n[2] - Campo elétrico");
 	    int escolha = sla.nextInt();
@@ -12,26 +22,39 @@ public class Main
 	        campoEletrico();
 	    }
 	    else{
-	        System.out.println("Por favor escolha um valor válido");
+	        System.out.println("Por favor escolha uma opção válida\n");
+	        escolha();
 	    }
 	}
+	
 	public static void cargaEletrica(){
 	    Scanner sla = new Scanner(System.in);
-	    System.out.println("Digite a quantidade de elétrons: ");
+	    try{
+	    System.out.println("\nDigite a quantidade de elétrons: ");
 	    double n = sla.nextDouble();
 	    double e = 1.6 * (Math.pow(10, -19));
 	    double resultado = n * e;
-	    System.out.println("O valor da carga elétrica é "+resultado);
+	    System.out.println("\nO valor da carga elétrica é "+resultado);
+	    }
+	    catch (Exception e){
+	        System.out.println("\nPor favor digite um valor válido");
+	        cargaEletrica();
+	    }
 	}
 	
 	public static void campoEletrico(){
 	    Scanner sla = new Scanner(System.in);
-	    System.out.println("Digite o valor da força elétrica: ");
+	    try{
+	    System.out.println("\nDigite o valor da força elétrica: ");
 	    double f = sla.nextDouble();
-	    System.out.println("Digite o valor da carga elétrica: ");
+	    System.out.println("\nDigite o valor da carga elétrica: ");
 	    double q = sla.nextDouble();
 	    double resultado = f/q;
-	    System.out.println("O valor do campo elétrico é "+resultado);
+	    System.out.println("\nO valor do campo elétrico é "+resultado);
+	    }
+	    catch (Exception e){
+	        System.out.println("\nPor favor digite um valor válido");
+	        campoEletrico();
+	    }
 	}
 }
-	
